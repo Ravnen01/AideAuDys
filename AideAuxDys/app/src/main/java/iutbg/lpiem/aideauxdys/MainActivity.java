de88520ac8c8c8833ffda64c15f27cb3d387bf13
Corentin LECOMTE <lecomte.corentin@gmail.com>
2016-01-06 11:19:11 +0100

    création activityTokenPhoto

--- AideAuxDys/app/src/main/java/iutbg/lpiem/aideauxdys/MainActivity.java
+++ AideAuxDys/app/src/main/java/iutbg/lpiem/aideauxdys/MainActivity.java
@@ -1,11 +1,21 @@
 package iutbg.lpiem.aideauxdys;
 
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.net.Uri;
 import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.ImageView;
 
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 public class MainActivity extends AppCompatActivity {
     public static final String PACKAGE_NAME = "iutbg.lpiem.aideauxdys";
@@ -13,6 +23,8 @@ public class MainActivity extends AppCompatActivity {
     public static final String lang = "fra";
     private static final String TAG = "MainActivity.java";
     protected static final String PHOTO_TAKEN = "photo_taken";
+    protected String path;
+    protected boolean taken;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -35,5 +47,80 @@ public class MainActivity extends AppCompatActivity {
 
         }
 
+        if (!(new File(DATA_PATH + "tessdata/" + lang + ".traineddata")).exists()) {
+            try {
+
+                AssetManager assetManager = getAssets();
+                InputStream in = assetManager.open("tessdata/" + lang + ".traineddata");
+                //GZIPInputStream gin = new GZIPInputStream(in);
+                OutputStream out = new FileOutputStream(DATA_PATH
+                        + "tessdata/" + lang + ".traineddata");
+
+                // Transfer bytes from in to out
+                byte[] buf = new byte[1024];
+                int len;
+                //while ((lenf = gin.read(buff)) > 0) {
+                while ((len = in.read(buf)) > 0) {
+                    out.write(buf, 0, len);
+                }
+                in.close();
+                //gin.close();
+                out.close();
+
+                Log.v(TAG, "Copied " + lang + " traineddata");
+            } catch (IOException e) {
+                Log.e(TAG, "Was unable to copy " + lang + " traineddata " + e.toString());
+            }
+        }
+        path = DATA_PATH + "/ocr.jpg";
+
+        ImageView ivPhoto=(ImageView)findViewById(R.id.ivPhoto);
+        ivPhoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startCameraActivity();
+            }
+        });
+
+    }
+
+    protected void startCameraActivity() {
+        File file = new File(path);
+        Uri outputFileUri = Uri.fromFile(file);
+
+        final Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, outputFileUri);
+
+        startActivityForResult(intent, 0);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.i(TAG, "resultCode: " + resultCode);
+
+        if (resultCode == -1) {
+            starActivityPhotoTaken();
+        } else {
+            Log.v(TAG, "User cancelled");
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putBoolean(MainActivity.PHOTO_TAKEN, taken);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        Log.i(TAG, "onRestoreInstanceState()");
+        if (savedInstanceState.getBoolean(MainActivity.PHOTO_TAKEN)) {
+            starActivityPhotoTaken();
+        }
+    }
+
+    protected void starActivityPhotoTaken(){
+        Intent intent=new Intent(this,TextTokenActivity.class);
+        startActivity(intent);
     }
 }
---
+++ AideAuxDys/app/src/main/java/iutbg/lpiem/aideauxdys/TextTokenActivity.java
@@ -0,0 +1,13 @@
+package iutbg.lpiem.aideauxdys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TextTokenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_text_token);
+    }
+}
