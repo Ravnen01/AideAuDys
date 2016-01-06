436322c0efb8b674337cb6c206fc8f7d6382b0ae
Corentin LECOMTE <lecomte.corentin@gmail.com>
2016-01-06 11:54:23 +0100

    création de l'asynctask de gestion de l'or

---
+++ AideAuxDys/app/src/main/java/iutbg/lpiem/aideauxdys/Task/PhotoTokenAssync.java
@@ -0,0 +1,126 @@
+package iutbg.lpiem.aideauxdys.Task;
+
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.os.AsyncTask;
+import android.os.Environment;
+import android.util.Log;
+
+import com.googlecode.tesseract.android.TessBaseAPI;
+
+import java.io.IOException;
+
+/**
+ * Created by Corentin on 06/01/2016.
+ */
+public class PhotoTokenAssync extends AsyncTask {
+    private static final String TAG = "PhotoTokenAssync.java";
+    public static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/AideAuxDysOCR/";
+    public static final String lang = "fra";
+    private Context context;
+    private String path;
+
+    public PhotoTokenAssync(Context context) {
+        this.context = context;
+        this.path= DATA_PATH + "/ocr.jpg";
+    }
+
+    @Override
+    protected Object doInBackground(Object[] params) {
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(path, options);
+
+        try {
+            ExifInterface exif = new ExifInterface(path);
+            int exifOrientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+
+            Log.v(TAG, "Orient: " + exifOrientation);
+
+            int rotate = 0;
+
+            switch (exifOrientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate = 270;
+                    break;
+            }
+
+            Log.v(TAG, "Rotation: " + rotate);
+
+            if (rotate != 0) {
+
+                // Getting width & height of the given image.
+                int w = bitmap.getWidth();
+                int h = bitmap.getHeight();
+
+                // Setting pre rotate
+                Matrix mtx = new Matrix();
+                mtx.preRotate(rotate);
+
+                // Rotating Bitmap
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+            }
+
+            // Convert to ARGB_8888, required by tess
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+        } catch (IOException e) {
+            Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+        }
+
+        // _image.setImageBitmap( bitmap );
+
+        Log.v(TAG, "Before baseApi");
+
+        TessBaseAPI baseApi = new TessBaseAPI();
+        baseApi.setDebug(true);
+        baseApi.init(DATA_PATH, lang);
+        baseApi.setImage(bitmap);
+
+        String recognizedText = baseApi.getUTF8Text();
+
+        baseApi.end();
+
+        // You now have the text in recognizedText var, you can do anything with it.
+        // We will display a stripped out trimmed alpha-numeric version of it (if lang is eng)
+        // so that garbage doesn't make it to the display.
+
+        Log.v(TAG, "OCRED TEXT: " + recognizedText);
+
+        if ( lang.equalsIgnoreCase("eng") ) {
+            recognizedText = recognizedText.replaceAll("[^a-zA-Z0-9]+", " ");
+        }
+
+        recognizedText = recognizedText.trim();
+
+        if ( recognizedText.length() != 0 ) {
+            return recognizedText;
+        }
+
+        // Cycle done.
+
+        return null;
+    }
+
+    @Override
+    protected void onPostExecute(Object o) {
+        super.onPostExecute(o);
+    }
+
+    @Override
+    protected void onCancelled() {
+        super.onCancelled();
+    }
+}
--- AideAuxDys/app/src/main/java/iutbg/lpiem/aideauxdys/TextTokenActivity.java
+++ AideAuxDys/app/src/main/java/iutbg/lpiem/aideauxdys/TextTokenActivity.java
@@ -1,13 +1,22 @@
 package iutbg.lpiem.aideauxdys;
 
+import android.os.AsyncTask;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import iutbg.lpiem.aideauxdys.Task.PhotoTokenAssync;
+
 public class TextTokenActivity extends AppCompatActivity {
+    public static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/AideAuxDysOCR/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_text_token);
+        PhotoTokenAssync photoTokenAssync=new PhotoTokenAssync(getApplicationContext());
+        photoTokenAssync.execute();
     }
+
+
 }
