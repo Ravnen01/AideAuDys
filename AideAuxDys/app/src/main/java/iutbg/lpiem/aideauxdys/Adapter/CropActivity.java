e2ba9a9c7b60f22ab87ece1b4746687d935bcbf5
Corentin LECOMTE <lecomte.corentin@gmail.com>
2016-01-12 11:51:31 +0100

    création corpActivity et liaison photo cropActivity

--- AideAuxDys/app/src/main/java/iutbg/lpiem/aideauxdys/Adapter/CropActivity.java
+++ AideAuxDys/app/src/main/java/iutbg/lpiem/aideauxdys/Adapter/CropActivity.java
@@ -1,21 +1,83 @@
 package iutbg.lpiem.aideauxdys.Adapter;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Matrix;
+import android.media.ExifInterface;
+import android.os.Environment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.edmodo.cropper.CropImageView;
+
+import java.io.IOException;
 
 import iutbg.lpiem.aideauxdys.R;
 
 public class CropActivity extends AppCompatActivity {
     private CropImageView cropPhoto;
+    private static final String TAG = "PhotoTokenAssync.java";
+    public static final String DATA_PATH = Environment.getExternalStorageDirectory().toString() + "/AideAuxDysOCR/";
+    private String path;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crop);
+        this.path= DATA_PATH + "/ocr.jpg";
+
+        BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inSampleSize = 4;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(path, options);
+
+        try {
+            ExifInterface exif = new ExifInterface(path);
+            int exifOrientation = exif.getAttributeInt(
+                    ExifInterface.TAG_ORIENTATION,
+                    ExifInterface.ORIENTATION_NORMAL);
+
+            Log.v(TAG, "Orient: " + exifOrientation);
+
+            int rotate = 0;
+
+            switch (exifOrientation) {
+                case ExifInterface.ORIENTATION_ROTATE_90:
+                    rotate = 90;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_180:
+                    rotate = 180;
+                    break;
+                case ExifInterface.ORIENTATION_ROTATE_270:
+                    rotate = 270;
+                    break;
+            }
+
+            Log.v(TAG, "Rotation: " + rotate);
+
+            if (rotate != 0) {
+
+                // Getting width & height of the given image.
+                int w = bitmap.getWidth();
+                int h = bitmap.getHeight();
+
+                // Setting pre rotate
+                Matrix mtx = new Matrix();
+                mtx.preRotate(rotate);
+
+                // Rotating Bitmap
+                bitmap = Bitmap.createBitmap(bitmap, 0, 0, w, h, mtx, false);
+            }
+
+            // Convert to ARGB_8888, required by tess
+            bitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true);
+
+        } catch (IOException e) {
+            Log.e(TAG, "Couldn't correct orientation: " + e.toString());
+        }
 
         cropPhoto = (CropImageView)findViewById(R.id.CropImageView_crop);
-        //cropImageView.setImageBitmap(photo);
+        cropPhoto.setImageBitmap(bitmap);
     }
 }
