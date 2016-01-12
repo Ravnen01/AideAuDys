37fe926cbf3b447eafec5cea8d47e5e985a9056f
Corentin LECOMTE <lecomte.corentin@gmail.com>
2016-01-12 09:02:11 +0100

    création  imageSizeActivity

---
+++ AideAuxDys/app/src/main/java/iutbg/lpiem/aideauxdys/ImageSizeActivity.java
@@ -0,0 +1,13 @@
+package iutbg.lpiem.aideauxdys;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ImageSizeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_image_size);
+    }
+}
